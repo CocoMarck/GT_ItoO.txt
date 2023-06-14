088779bd2305a7eadb770c1785fb5409c12d99cb
CocoMarck <76503881+CocoMarck@users.noreply.github.com>
2023-06-14 03:40:13 -0600

    Add files via upload

--- Execute.bat
+++ Execute.bat
@@ -1,21 +1,21 @@
 cls
 
 
-echo ./GT_ID.exe -h
+echo .\GT_ID.exe -h
 
-./GT_ID.exe -h
+.\GT_ID.exe -h
 pause
 
 
 cls
-echo ./GT_ID.exe -i ./Archivos_base/ -id 10.1 -li es -lo en -o ./Archivos_traducidos/'
+echo .\GT_ID.exe -i .\Archivos_base\ -id 10.1 -li es -lo en -o .\Archivos_traducidos\
 
-./GT_ID.exe -i ./Archivos_base/ -id 10.1 -li es -lo en -o ./Archivos_traducidos/'
+.\GT_ID.exe -i .\Archivos_base\ -id 10.1 -li es -lo en -o .\Archivos_traducidos\
 pause
 
 
 cls
-echo ./GT_ID.exe -id 10 -li es -lo en
+echo .\GT_ID.exe -id 10 -li es -lo en
 
-./GT_ID.exe -id 10 -li es -lo en
+.\GT_ID.exe -id 10 -li es -lo en
 pause
