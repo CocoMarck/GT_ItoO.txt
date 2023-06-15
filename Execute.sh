91306151f1ac9bb2cfca01beabc399387dbc4465
CocoMarck <76503881+CocoMarck@users.noreply.github.com>
2023-06-14 20:00:39 -0600

    Add files via upload

--- Execute.sh
+++ Execute.sh
@@ -6,7 +6,7 @@ read
 
 
 clear
-app='python3 ./GT_ID.py -i ./Archivos_base/ -id 10.1 -li es -lo en -o ./Archivos_traducidos/'
+app="python3 ./GT_ID.py -i ./Archivos_base/ -id 10.1 -li es -lo en -o ./Archivos_traducidos/"
 
 echo $app
 $app
@@ -14,7 +14,37 @@ read
 
 
 clear
-app0='python3 ./GT_ID.py -id 10 -li es -lo en'
-echo $app0
-$app0
+app="python3 ./GT_ID.py -id 10 -li es -lo en"
+echo $app
+$app
+read
+
+
+clear
+
+echo python3 ./GT_ID.py -t "Texto necesario" -li es -lo en
+
+python3 ./GT_ID.py -t "Texto necesario" -li es -lo en
+read
+
+
+clear
+
+echo python3 ./GT_ID.py -t "Texto necesario" -li es -lo en -o ./OnlyText_en.txt
+
+python3 ./GT_ID.py -t "Texto necesario" -li es -lo en -o ./OnlyText_en.txt
+read
+
+
+clear
+app="python3 ./GT_ID.py -i ./Texto_es.txt -li es -lo en"
+echo $app
+$app
+read
+
+
+clear
+app="python3 ./GT_ID.py -i ./Texto_es.txt -li es -lo en -o ./Text_en.txt"
+echo $app
+$app
 read
