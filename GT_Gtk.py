e0d1431a73e8245b527643fc570c932a0eb65494
CocoMarck <76503881+CocoMarck@users.noreply.github.com>
2023-11-23 20:12:09 -0800

    Add files via upload

--- GT_Gtk.py
+++ GT_Gtk.py
@@ -1,3 +1,7 @@
+from os.path import isfile
+from Modulos.Modulo_Text import (
+    Text_Read
+)
 from Modulos.Modulo_Language import (
     get_text as Lang,
     Default_Language
@@ -17,47 +21,6 @@ class Window_Main(Gtk.Window):
         self.set_default_size(308, -1)
         
         # Contenedor Principal
-        vbox_main = Gtk.Box(
-            orientation=Gtk.Orientation.VERTICAL, spacing=8
-        )
-        
-        # Secciones Vertical - Opciones
-        self.button_set_file = Gtk.Button( label=Lang('set_arch') )
-        self.button_set_file.connect('clicked', self.evt_set_file)
-        vbox_main.pack_start(self.button_set_file, True, False, 0)
-        
-        self.button_set_text = Gtk.Button( label=Lang('set_txt') )
-        self.button_set_text.connect('clicked', self.evt_set_text)
-        vbox_main.pack_start(self.button_set_text, True, False, 0)
-        
-        # Fin - Agregar contenedor principal
-        self.add(vbox_main)
-    
-    def evt_set_file(self, widget):
-        self.hide()
-        dialog = Dialog_translator_file(self)
-        dialog.run()
-        dialog.destroy()
-        self.show_all()
-
-    def evt_set_text(self, widget):
-        self.hide()
-        dialog = Dialog_translator_txt(self)
-        dialog.run()
-        dialog.destroy()
-        self.show_all()
-
-
-class Dialog_translator_file(Gtk.Dialog):
-    def __init__(self, parent):
-        super().__init__(
-            title=Lang('trs_file'),
-            transient_for=parent, flags=0
-        )
-        self.set_resizable(True)
-        self.set_default_size(512, -1)
-        
-        # Contenedor Principal
         vbox_main = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=8)
         vbox_main.set_property('expand', True)
         
@@ -74,75 +37,17 @@ class Dialog_translator_file(Gtk.Dialog):
         button_i_dir.connect('clicked', self.evt_set_input_dir)
         hbox.pack_end(button_i_dir, False, True, 0 )
         
-        # Sección Vertical - Establecer Language input y output
-        hbox = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=4)
-        vbox_main.pack_start(hbox, True, False, 0)
-        
-        label = Gtk.Label(label=f'{Lang("lang")}: ')
-        hbox.pack_start(label, False, True, 8)
-        
-        self.entry_i_lang = Gtk.Entry(
-            text=str(Default_Language()),
-            placeholder_text=Lang('i')
+        # Sección vertical - Texto en un Text View
+        scroll_win = Gtk.ScrolledWindow(
+            hexpand=True,
+            vexpand=True
         )
-        hbox.pack_start(self.entry_i_lang, False, True, 0)
+        vbox_main.pack_start(scroll_win, True, True, 0)
         
-        self.entry_o_lang = Gtk.Entry(
-            text='',
-            placeholder_text=Lang('o')
+        self.text_view = Gtk.TextView(
+            editable=True
         )
-        hbox.pack_end(self.entry_o_lang, False, True, 0)
-        
-        # Seccion vertical - Iniciar traducción
-        button_start_translate = Gtk.Button(
-            label = Lang('start')
-        )
-        vbox_main.pack_start(button_start_translate, True, False, 8)
-        
-        # Fin - Establecer todo
-        self.get_content_area().add(vbox_main)
-        self.show_all()
-    
-    def evt_set_input_dir(self, widget):
-        dialog = Gtk.FileChooserDialog(
-            parent=self,
-            title=Lang('set_dir'),
-            action=Gtk.FileChooserAction.OPEN
-        )
-        dialog.add_buttons(
-            Gtk.STOCK_CANCEL,
-            Gtk.ResponseType.CANCEL,
-            Lang('set'),
-            Gtk.ResponseType.OK
-        )
-        dialog.set_current_folder( self.entry_i_dir.get_text() )
-
-        filter_txt = Gtk.FileFilter()
-        filter_txt.set_name( Lang('text') )
-        filter_txt.add_mime_type('text/plain')
-        dialog.add_filter(filter_txt)
-        
-        response = dialog.run()
-        if response == Gtk.ResponseType.OK:
-            self.entry_i_dir.set_text( dialog.get_filename() )
-        elif response == Gtk.ResponseType.CANCEL:
-            pass
-        
-        dialog.destroy()
-
-
-class Dialog_translator_txt(Gtk.Dialog):
-    def __init__(self, parent):
-        super().__init__(
-            title=Lang('trs_txt'),
-            transient_for=parent, flags=0
-        )
-        self.set_resizable(True)
-        self.set_default_size(308, -1)
-        
-        # Contenedor Principal
-        vbox_main = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=8)
-        vbox_main.set_property('expand', True)
+        scroll_win.add( self.text_view )
         
         # Sección Vertical - Establecer Language input y output
         hbox = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=4)
@@ -167,11 +72,100 @@ class Dialog_translator_txt(Gtk.Dialog):
         button_start_translate = Gtk.Button(
             label = Lang('start')
         )
+        button_start_translate.connect('clicked', self.evt_start_translate)
         vbox_main.pack_start(button_start_translate, True, False, 8)
         
-        # Fin - Establecer todo
-        self.get_content_area().add(vbox_main)
-        self.show_all()
+        # Fin - Agregar contenedor principal
+        self.add(vbox_main)
+    
+    def evt_set_input_dir(self, widget):
+        # Establecer un archivo de texto, y si se logra detectar texto, establecerlo en el text view
+
+        dialog = Gtk.FileChooserDialog(
+            parent=self,
+            title=Lang('set_dir'),
+            action=Gtk.FileChooserAction.OPEN
+        )
+        dialog.add_buttons(
+            Gtk.STOCK_CANCEL,
+            Gtk.ResponseType.CANCEL,
+            Lang('set'),
+            Gtk.ResponseType.OK
+        )
+        dialog.set_current_folder( self.entry_i_dir.get_text() )
+
+        filter_txt = Gtk.FileFilter()
+        filter_txt.set_name( Lang('text') )
+        filter_txt.add_mime_type('text/plain')
+        dialog.add_filter(filter_txt)
+        
+        response = dialog.run()
+        if response == Gtk.ResponseType.OK:
+            self.entry_i_dir.set_text( dialog.get_filename() )
+            if isfile(dialog.get_filename()):
+                text = Text_Read(
+                    file_and_path=dialog.get_filename(),
+                    option='ModeText'
+                )
+                self.text_view.get_buffer().set_text(text)
+            
+        elif response == Gtk.ResponseType.CANCEL:
+            pass
+        
+        dialog.destroy()
+    
+    def evt_start_translate(self, widget):
+        # Verificar que los parametros esten correctos
+        save = True
+        
+        buffer_text_view = self.text_view.get_buffer()
+        text_input = buffer_text_view.get_text(
+            buffer_text_view.get_start_iter(),
+            buffer_text_view.get_end_iter(),
+            False
+        )
+        if text_input == '':
+            print(f'ERROR - {Lang("text")}')
+            save = False
+        
+        if self.entry_i_lang.get_text() == '':
+            print(f'ERROR - {Lang("lang")} | {Lang("i")}')
+            save = False
+            
+        if self.entry_o_lang.get_text() == '':
+            print(f'ERROR - {Lang("lang")} | {Lang("o")}')
+            save = False
+        
+        # Fin - Traducir
+        if save == True:
+            print(Lang('finalized'))
+            
+            dialog = Gtk.FileChooserDialog(
+                title=Lang('save_arch'), parent=self,
+                action=Gtk.FileChooserAction.SAVE
+            )
+            dialog.add_buttons(
+                Gtk.STOCK_CANCEL,
+                Gtk.ResponseType.CANCEL,
+                Gtk.STOCK_SAVE,
+                Gtk.ResponseType.OK
+            )
+            dialog.set_current_name( f"{Lang('name')}.txt" )
+
+            filter_txt = Gtk.FileFilter()
+            filter_txt.set_name( Lang('text') )
+            filter_txt.add_mime_type('text/plain')
+            dialog.add_filter(filter_txt)
+            
+            response = dialog.run()
+            if response == Gtk.ResponseType.OK:
+                print( dialog.get_filename() )
+            elif response == Gtk.ResponseType.CANCEL:
+                pass
+            
+            dialog.destroy()
+            
+            print(text_input)
 
 
 win = Window_Main()
